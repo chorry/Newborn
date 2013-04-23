8e42bdae086e2f54b8ef0ae4bc608dce123d19d6
Alexander Loutsenko <alex.chorry@gmail.com>
2013-04-23 18:04:50 +0400

    overhaul pt.2

--- README.md
+++ README.md
@@ -9,6 +9,20 @@ Autohosts
 ---------
 * Custom hosts goes to /var/www/%foldername% + you have to make custom vhosts config for apache and a2ensite %configname%
   Hosts are available as %yourhostname%.dev
-* Auto hosts goes to /var/www/autohosts/%foldername%/_  (yep, doent forget underscore)
-  Hosts are available as %foldername%.auto.dev
+* Auto hosts goes to /var/www/autohosts/%foldername%/
+
+
+
+Example setup
+-------------
+Host:
+* served by apache:
+** http://example.dev:8000/
+** http://www-masters.example.dev
+
+* served by nginx
+** http://example.dev/    (direct proxy to apache master)
+** http://example.dev:81/ (slave1 proxy to master)
+** http://example.dev:82/ (slave2 cache to slave1)
+
 
--- webServers.sh
+++ webServers.sh
@@ -4,24 +4,27 @@ add-apt-repository -y ppa:nginx/$nginx
 apt-get update 
 apt-get install -y nginx
 
-#Php and  Apache 2
+#Php and  Apache2
 apt-get install -y php5 php5-common php5-curl php-pear php5-dev php5-mysql php5-sqlite php-apc libapache2-mod-rpaf libapache2-mod-php5 php5-suhosin php5-gd php5-fpm php5-cgi php5-mcrypt
 sed -e 's/80/8000/' /etc/apache2/ports.conf > /etc/apache2/ports.conf.tmp && mv  /etc/apache2/ports.conf.tmp /etc/apache2/ports.conf
 sed -e 's/Listen 8000/Listen 127.0.0.1:8000/' /etc/apache2/ports.conf > /etc/apache2/ports.conf.tmp && mv  /etc/apache2/ports.conf.tmp /etc/apache2/ports.conf
 
-#copy and link configs
+#copy configs
 mkdir /var/www/autohost/
-cp apache2-configs/autoHost.loc /etc/apache2/sites-available/
-cp nginx-configs/apacheProxy.loc /etc/nginx/sites-available/
-cp -r nginx-configs/includes /etc/nginx/
-rm /etc/nginx/sites-enabled/*
-ln -s /etc/nginx/sites-available/apacheProxy.loc /etc/nginx/sites-enabled/apacheProxy.loc
+cp example-site-configs/apache2/* /etc/apache2/sites-available/
+cp example-site-configs/nginx/* /etc/nginx/sites-available/
+cp -r example-server-configs/nginx/includes/* /etc/nginx/
+cp -r example-server-configs/nginx/conf.d/* /etc/nginx/conf.d/
 
 #enable stuff
 a2enmod vhost_alias
 a2dissite default
 a2ensite autoHost.loc
 
+rm /etc/nginx/sites-enabled/*
+ln -s /etc/nginx/sites-available/apacheProxy.loc /etc/nginx/sites-enabled/apacheProxy.loc
+ln -s /etc/nginx/sites-available/nginx.slave1.conf /etc/nginx/sites-enabled/nginx.slave1.conf
+ln -s /etc/nginx/sites-available/nginx.slave2.conf /etc/nginx/sites-enabled/nginx.slave2.conf
 
 #nginx stuff
 mkdir /var/log/nginx/example-master1/
@@ -42,6 +45,7 @@ mkdir /var/www/nginx-fastcgi-cache/example.dev-s1
 mkdir /var/www/nginx-fastcgi-cache/temp-s1
 mkdir /var/www/nginx-fastcgi-cache/temp-s2
 
+cp -r example-sites-www/* /var/www/
 #fpm stuff
 sed -e 's/listen = \/var\/run\/php5-fpm.sock/\;listen = \/var\/run\/php5-fpm.sock\nlisten=127.0.0.1:9000/' /etc/php5/fpm/pool.d/www.conf > /etc/php5/fpm/pool.d/www.conf.bak && mv /etc/php5/fpm/pool.d/www.conf.bak /etc/php5/fpm/pool.d/www.conf
 service php5-fpm restart
