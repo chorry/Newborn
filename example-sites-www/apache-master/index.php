30a8031134e387eb42e36ca68e3b98fdf2b02c3e
Alexander Loutsenko <alex.chorry@gmail.com>
2013-04-23 18:03:35 +0400

    major dir overhaul

---
+++ example-sites-www/apache-master/index.php
@@ -0,0 +1,2 @@
+<?php
+echo 'Hola from apache-master @'.$_SERVER['HTTP_HOST'];
---
+++ example-sites-www/nginx-slave1/htdocs/index.php
@@ -0,0 +1,5 @@
+<?php
+print 'Hello from slave1 @'.$_SERVER['HTTP_HOST']."<br>";
+print "Check how cache is working: ".rand()."<- this value should change only after 5 seconds (default value) after first request<br>";
+print "And here is phpinfo for you:<br>";
+phpinfo();
